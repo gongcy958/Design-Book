7deb6bebab6f3f27a7507a4d059e315a45f410b7
gongcy <gongchengyao9797@126.com>
2022-09-02 18:02:58 +0800

    optimize

--- src/main/java/behaviour/observer/Demo.java
+++ src/main/java/behaviour/observer/Demo.java
@@ -1,5 +1,7 @@
 package behaviour.observer;
 
+import java.util.concurrent.*;
+
 /**
  * @author gongcy
  * @date 2022/8/29 5:47 下午
@@ -7,10 +9,29 @@ package behaviour.observer;
  */
 public class Demo {
 
-    public static void main(String[] args) {
-        ConcreteSubject subject = new ConcreteSubject();
-        subject.registerObserver(new ConcreteObserverOne());
-        subject.registerObserver(new ConcreteObserverTwo());
-        subject.notifyObservers(new Message());
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        ConcreteSubject subject = new ConcreteSubject();
+//        subject.registerObserver(new ConcreteObserverOne());
+//        subject.registerObserver(new ConcreteObserverTwo());
+//        subject.notifyObservers(new Message());
+
+        ScheduledExecutorService scheduled = Executors.newScheduledThreadPool(5);
+        ScheduledFuture<?> future = scheduled.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("开始执行任务");
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println("执行完毕");
+            }
+        }, 1000L, 1000L, TimeUnit.MILLISECONDS);
+        System.out.println("阻塞开始");
+        System.out.println(future.get() + "");
+        System.out.println("阻塞结束");
+
+
     }
 }
